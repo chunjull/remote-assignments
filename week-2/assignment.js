821927486c017f8c7b6d882471e764a8e743b35e
chunjull <chunjuchein@gmail.com>
2024-06-24 09:43:23 +0800

    feat: week-2 build environment

---
+++ week-2/assignment.js
@@ -0,0 +1,76 @@
+/* 
+Assignment 1: Function and Array
+Complete the function below to find the max number of the passing array of numbers.
+*/
+
+/* Example
+function max(numbers) {
+	// your code here
+}
+console.log(max([1, 2, 4, 5])); // expected output: 5
+console.log(max([5, 2, 7, 1, 6])); // expected output: 7
+*/
+
+
+/*
+Assignment 2: Function and Object
+Complete the function below to calculate the result of the passing object.
+*/
+
+/* Example
+function calculate(args) {
+	// your code here
+}
+console.log(calculate({ n1: 2, n2: 3, op: '+' })); // expected output: 5
+console.log(calculate({ n1: 5, n2: 2, op: '-' }));  // expected output: 3
+console.log(calculate({ n1: 1, n2: 6, op: 'x' }));  // expected output: 'Not supported'
+*/
+
+/*
+Assignment 3: Function, Array, and Object
+Complete the function below to calculate the total price of all products after applying a discount.
+*/
+
+/* Example
+function calculate(data) {
+	// your code here
+}
+const discountedPrice = calculate({
+	discount: 0.1,
+	products: [
+		{
+			name: "Product 1",
+			price: 100
+		},
+		{
+			name: "Product 2",
+			price: 700
+		},
+		{
+			name: "Product 3",
+			price: 250
+		}
+	]
+});
+console.log(discountedPrice) // show the total price of all products after applying a discount
+*/
+
+
+/*
+Assignment 5: Algorithm (Advanced Optional)
+Given an array of integers, return indices of the two numbers such that they add up to a specific target. You may assume that each input would have exactly one solution, and you may not use the same element twice.
+*/
+
+/* Example
+function twoSum(nums, target) {
+	// your code here
+}
+  
+/*
+	For example:
+		twoSum([2, 7, 11, 15], 9);
+	Should returns:
+		[0, 1]
+	Because:
+		nums[0] + nums[1] is 9
+*/
