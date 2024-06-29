b700b7f3509a210bc9d66783e693bbedca3af5fe
chunjull <chunjuchein@gmail.com>
2024-06-29 15:42:44 +0800

    feat: update week-2 assignment

--- week-2/assignment.js
+++ week-2/assignment.js
@@ -2,8 +2,6 @@
 Assignment 1: Function and Array
 Complete the function below to find the max number of the passing array of numbers.
 */
-
-/* Example */
 function max(numbers) {
 	let maxNum = numbers[0];
 	for (let i = 0; i < numbers.length; i++) {
@@ -12,7 +10,6 @@ function max(numbers) {
 		} 
 	}return maxNum;
 }
-
 console.log(max([1, 2, 4, 5])); // expected output: 5
 console.log(max([5, 2, 7, 1, 6])); // expected output: 7
 
@@ -20,44 +17,59 @@ console.log(max([5, 2, 7, 1, 6])); // expected output: 7
 Assignment 2: Function and Object
 Complete the function below to calculate the result of the passing object.
 */
-
-/* Example
 function calculate(args) {
-	// your code here
+	let n1 = args.n1;
+	let n2 = args.n2;
+	let op = args.op;
+
+	if (op === '+') {
+		return n1 + n2;
+	} else if (op === '-') {
+		return n1 - n2;
+	} else if (op === '*') {
+		return n1 * n2;
+	} else if (op === '/') {
+		return n1 / n2;
+	} else {
+		return "Not supported";
+	};
 }
 console.log(calculate({ n1: 2, n2: 3, op: '+' })); // expected output: 5
 console.log(calculate({ n1: 5, n2: 2, op: '-' }));  // expected output: 3
 console.log(calculate({ n1: 1, n2: 6, op: 'x' }));  // expected output: 'Not supported'
-*/
 
 /*
 Assignment 3: Function, Array, and Object
 Complete the function below to calculate the total price of all products after applying a discount.
 */
-
-/* Example
 function calculate(data) {
-	// your code here
+  let discount = data.discount;
+  let products = data.products;
+  let totalPrice = 0;
+  for (let i = 0; i < products.length; i++) {
+    totalPrice += products[i].price;
+  }
+  let discountedPrice = totalPrice * (1 - discount);
+  return discountedPrice;
 }
 const discountedPrice = calculate({
-	discount: 0.1,
-	products: [
-		{
-			name: "Product 1",
-			price: 100
-		},
-		{
-			name: "Product 2",
-			price: 700
-		},
-		{
-			name: "Product 3",
-			price: 250
-		}
-	]
+  discount: 0.1,
+  products: [
+    {
+      name: "Product 1",
+      price: 100
+    },
+    {
+      name: "Product 2",
+      price: 700
+    },
+    {
+      name: "Product 3",
+      price: 250
+    }
+  ]
 });
-console.log(discountedPrice) // show the total price of all products after applying a discount
-*/
+console.log(discountedPrice);
 
 
 /*
--- week-2/index.js
+++ week-2/index.js
@@ -1,7 +1,29 @@
 /* 1. Click to Change Text. */
 const textChange = document.querySelector('#text-change');
 textChange.addEventListener("click", function() {
-  textChange.textContent = "Have a Good Time!";
+  textChange.textContent = "HAVE a GOOD TIME!";
+});
+
+/* 2. Click to Show/Close Menu. */
+const toggle = document.querySelector(".toggle-button");
+const navbar = document.querySelector(".navbar");
+const bar = document.querySelectorAll(".bar");
+toggle.addEventListener("click", () => {
+  navbar.classList.toggle("active");
+  for (let i = 0; i < bar.length; i++) {
+    bar[i].classList.toggle("active");
+  }
 });
 
 /* 3. Click to Show More Content Boxes. */
+const btn = document.getElementById("btn-toggle");
+btn.addEventListener("click", () => {
+  const hiddenCard = document.querySelector(".d-none");
+  if (hiddenCard.style.display === "none") {
+    btn.textContent = "Show less"
+    hiddenCard.style.display = "flex";
+  } else {
+    btn.textContent = "Show more"
+    hiddenCard.style.display = "none";
+  }
+});
