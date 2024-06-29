063e4e3864d055f5945bf89f843f21111578b1bf
chunjull <chunjuchein@gmail.com>
2024-06-29 23:50:36 +0800

    feat: update week-2 advanced assignment

--- week-2/assignment.js
+++ week-2/assignment.js
@@ -76,12 +76,16 @@ console.log(discountedPrice);
 Assignment 5: Algorithm (Advanced Optional)
 Given an array of integers, return indices of the two numbers such that they add up to a specific target. You may assume that each input would have exactly one solution, and you may not use the same element twice.
 */
-
-/* Example
 function twoSum(nums, target) {
-	// your code here
+	for (let i = 0; i < nums.length; i++) {
+		for (let j = i + 1; j < nums.length; j++) {
+			if (nums[i] + nums[j] === target) {
+				return [i, j];
+			}
+		}
+	}
+	return [];
 }
-  
 /*
 	For example:
 		twoSum([2, 7, 11, 15], 9);
