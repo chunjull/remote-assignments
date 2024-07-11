2a25d0c8d4d01bb7310eca1c20f82da781db660f
chunjull <chunjuchein@gmail.com>
2024-07-11 10:47:23 +0800

    feat: week-4 assignment-3 responsive navbar

--- week-4/assignment-3/src/components/Header.jsx
+++ week-4/assignment-3/src/components/Header.jsx
@@ -1,12 +1,21 @@
+import { useRef } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+
 function Header() {
   const Link = props => <a {...props}>Star Explorer</a>;
+  const navRef = useRef();
+  const showNavBar = () => {
+    navRef.current.classList.toggle("active");
+  }
+
 
   return (
     <div className="header-bg">
       <div className="container">
         <header className="header">
           <h1><Link href="/index.html"></Link></h1>
-          <nav className="navbar">
+          <nav className="navbar" ref={navRef}>
+            <button className="navBtn closeBtn" onClick={showNavBar}><FaTimes/></button>
             <ul className="menu">
               <li><button>About</button></li>
               <li><button>Plan</button></li>
@@ -14,6 +23,9 @@ function Header() {
               <li><button>FAQ</button></li>
             </ul>
           </nav>
+          <button className="navBtn" onClick={showNavBar}>
+            <FaBars />
+          </button>
         </header>
       </div>
     </div>
