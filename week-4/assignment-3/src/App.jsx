9247913af424662b5165b3cc57844c0e287378fb
chunjull <chunjuchein@gmail.com>
2024-07-12 17:05:46 +0800

    feat: week-4 assignment-3 source code

---
+++ week-4/assignment-3/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from "./components/Header";
+import Banner from "./components/Banner";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+    <Header />
+    <Banner />
+    <Main />
+    <Footer />
+    </>
+  );
+}
+
+export default App;
